51357148248d42a34518db7f25d0a6d18420c4fd
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2024-11-27 15:22:41 -0300

    Muito bem to comprendendo

--- Desafio/script.js
+++ Desafio/script.js
@@ -1,19 +1,34 @@
-const apiKey = '45bd03bc75c98e54c9f31cdb0a964f3d'; // Substitua com sua chave da OpenWeatherMap
+const apiKey = '706e6c2535844f60b8b192311242511'; // Substitua com sua chave da OpenWeatherMap
 
 document.getElementById('weatherForm').addEventListener('submit', function(event) {
   event.preventDefault();
   
   const city = document.getElementById('city').value;
-  const apiUrl = `https://www.weatherapi.com/api-explorer.aspx`;
+  const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&lang=pt`;
+  const icon = `https://www.weatherapi.com/docs/weather_conditions.json`
 
+  
   fetch(apiUrl)
     .then(response => {
       if (!response.ok) throw new Error('Cidade não encontrada');
       return response.json();
     })
     .then(data => {
-      const temperature = data.main.temp;
-      document.getElementById('result').innerText = `A temperatura atual em ${city} é ${temperature}°C.`;
+      const temperature = data.current.temp_c;
+      const sensacaoTermica = data.current.windchill_c;
+      const humidade = data.current.humidity;
+      const vento = data.current.wind_kph;
+      const icone = data.current.condition.icon;
+      const condicao= data.current.condition.text;
+
+      document.getElementById('result').innerHTML = `
+      <img src="https:${icone}" alt="${condicao}" />
+      \nDados da Cidade: ${city} 
+      \nTemperatura atual: ${temperature}°C.
+      \nSensação termica: ${sensacaoTermica}°C.
+      \nHumidade: ${humidade}%.
+      \nVento: ${vento}km/h.
+      `
     })
     .catch(error => {
       document.getElementById('result').innerText = `Erro: ${error.message}`;
