41471cc402d8402bfb292b187889acbb83b8dbe1
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2024-12-02 16:59:56 -0300

    feito

--- Desafio/script.js
+++ Desafio/script.js
@@ -5,7 +5,7 @@ document.getElementById('weatherForm').addEventListener('submit', function(event
   
   const city = document.getElementById('city').value;
   const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&lang=pt`;
-
+  const foreUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=4&aqi=yes&alerts=yes&lang=pt`
 
   
   fetch(apiUrl)
@@ -14,6 +14,7 @@ document.getElementById('weatherForm').addEventListener('submit', function(event
       return response.json();
     })
     .then(data => {
+      
       const temperature = data.current.temp_c;
       const sensacaoTermica = data.current.windchill_c;
       const humidade = data.current.humidity;
@@ -23,17 +24,67 @@ document.getElementById('weatherForm').addEventListener('submit', function(event
 
       document.getElementById('result').innerHTML = `
       <div>
+      <div id ="ajuste">
       <img src="https:${icone}" alt="${condicao}"/>
-      <p>${condicao}</p>
+      <h2>${condicao}</h2>
       </div>
       <h2>Dados da Cidade: ${city}</h2>
       <p>Temperatura atual: ${temperature}°C<p>
       <p>Sensação termica: ${sensacaoTermica}°C<p>
       <p>Humidade: ${humidade}%<p>
       <p>Vento: ${vento}km/h<p>
+      </div>
       `
     })
     .catch(error => {
       document.getElementById('result').innerText = `Erro: ${error.message}`;
     });
+
+
+fetch(foreUrl)
+.then(response => {
+  if (!response.ok) throw new Error('Cidade não encontrada');
+  return response.json();
+})
+.then(data => {
+  const forecastDays = data.forecast.forecastday;
+  let forecastHtml = ``;
+
+  const contador = 0;
+
+
+  
+  forecastDays.slice(1).forEach(day => {
+    const date = day.date;
+    const { maxtemp_c, mintemp_c, condition, maxwind_kph } = day.day;
+    const icone = condition.icon;
+    const condicao = condition.text;
+
+    forecastHtml += `
+      <div>
+      <div id = "Centralizado"> 
+        <h3>${date}</h3>
+        <img src="https:${icone}" alt="${condicao}" />
+        <p>Condição: ${condicao}</p>
+       </div>
+        <p>Temperatura Máxima: ${maxtemp_c}°C</p>
+        <p>Temperatura Mínima: ${mintemp_c}°C</p>
+        <p>Vento Máximo: ${maxwind_kph} km/h</p>
+      </div>
+    `;
+  });
+
+  document.getElementById('resultDeOutrosDias').innerHTML = forecastHtml;
+})
+.catch(error => {
+  document.getElementById('resultDeOutrosDias').innerText = `Erro: ${error.message}`;
 });
+});
+
+
+
+
+
+
+
+
