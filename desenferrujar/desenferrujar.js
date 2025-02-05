2d550a0e021e7f79634b3735bfbfb23d6812e71b
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-02-05 15:47:14 -0300

    maisMenos

---
+++ desenferrujar/desenferrujar.js
@@ -0,0 +1,13 @@
+function MudarTexto(){
+
+    let d1 = document.getElementById("d1")
+    let d2 = document.getElementById("d2")
+    let d3 = document.getElementById("d3")
+
+    d1.innerHTML = "KAUÃ 1"
+    d2.innerHTML = "KAUÃ 2"
+    d3.innerHTML = "KAUÃ 3"
+
+
+}
+
